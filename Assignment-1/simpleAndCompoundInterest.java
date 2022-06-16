ac772607fa672b2804acd21c8212c90def8f882d
Raut <saurabh.raut@capgemini.com>
2022-06-16 15:59:03 +0530

    Compount Interest fun is added

--- Assignment-1/simpleAndCompoundInterest.java
+++ Assignment-1/simpleAndCompoundInterest.java
@@ -4,27 +4,36 @@ import java.util.Scanner;
  * simpleAndCompoundInterest
  */
 public class  simpleAndCompoundInterest {
-    public static double SimpleInterest(int p, int r, int t) {
-        double res =  (p * r * t)/100;
-        System.out.println(res);
+    public static double SimpleInterest(int p, Float r, int t) {
+        // formula is SI = (principle * rate * time)/100
+        double res =  (p * r * t)/100; 
         return res;
         
     } 
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static double compoundInterest(int p, Float r, int t) {
         
+        //formula  CI = principle * (1+rate/100)^time - principle
+        double rate = r;
+        double time = t;
+        double temp = Math.pow((1+ rate/100), time);  
+        double res = p * (temp-1) ; 
+        return res;
+
+
+    }
+
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
         System.out.print("Ente the principle value : ");
         int principle = sc.nextInt();
         System.out.print("Enter the Rate Of interest : ");
-        int rate = sc.nextInt();
+        float rate = sc.nextFloat();
         System.out.print("Enter the Time : ");
-        int time = sc.nextInt();
-
-        System.out.println(principle+" "+rate+" "+time);
-         
+        int time = sc.nextInt();  
         System.out.println( "Simple interest is : "+ SimpleInterest(principle, rate, time));
+        System.out.printf( "Simple interest is %.2f : ", compoundInterest(principle, rate, time));
 
+        
         
     }
 }
