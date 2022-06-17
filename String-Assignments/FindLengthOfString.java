3df6d7888de1af1a56fdcdb2075343bc4e65d2ac
Raut <saurabh.raut@capgemini.com>
2022-06-18 00:18:24 +0530

    String assignments are commited

---
+++ String-Assignments/FindLengthOfString.java
@@ -0,0 +1,11 @@
+/**
+ * FindLengthOfString
+ */
+public class FindLengthOfString {
+
+    public static void main(String[] args) {
+        String str = "Hello World"; 
+	System.out.println(str.length());
+
+    }
+}
