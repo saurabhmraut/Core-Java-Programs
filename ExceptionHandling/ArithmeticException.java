46d7cfd4b6df07d4db5eff64f454642823650993
Raut <saurabh.raut@capgemini.com>
2022-06-18 18:00:51 +0530

    Unsuported operation error

--- ExceptionHandling/ArithmeticException.java
+++ ExceptionHandling/ArithmeticException.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner; 
 
 /**
  * ArithmeticException
@@ -12,12 +14,28 @@ public class ArithmeticException {
             System.out.println("Enter the first value");
             int b = sc.nextInt();
             int c = a/b;
-            System.out.println("Result = " + c);
+            System.out.println("Result = " + c); 
+
+            //Q 2 
+            String array[] = {"a", "b", "c"};
+            List<String> list = Arrays.asList(array);
+            list.add("d"); 
+        
         }
         catch (java.lang.ArithmeticException e)
         {
             System.out.println("Cannot divide the number by zero");
+        }  catch (UnsupportedOperationException e) {
+            System.out.println("Unsupported Operation Exception occurs");
         }
+
+        // try {
+        //     String array[] = {"a", "b", "c"};
+        //     List<String> list = Arrays.asList(array);
+        //     list.add("d"); 
+        // } catch (UnsupportedOperationException e) {
+        //     System.out.println("Unsupported Operation Exception occurs");
+        // }
 
     }
     
