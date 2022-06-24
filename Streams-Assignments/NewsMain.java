0a5645fdd50281da1151ddb36b527d2b6ecda033
Raut <saurabh.raut@capgemini.com>
2022-06-24 10:43:28 +0530

    Java11 Assignments commited

--- Streams-Assignments/NewsMain.java
+++ Streams-Assignments/NewsMain.java
@@ -13,36 +13,37 @@ public class NewsMain {
 
   public static void main(String[] args) {
     List<News> newsList = Arrays.asList(new News(100, "Saurabh Raut", "Asim", "it's a news about Indian politics"),
-    new News(101, "Rajnikanth", "Amar", "I watched your movie"),
+    new News(101, "Rajnikanth", "Mahesh", "I watched your movie"),
     new News(101, "Akshay Kumar", "Rajesh", " Your movies are of low Budget"),
     new News(103, "Lakshya", "Mahesh", "Your movies are very funny and low Budget"),
     new News(101, "Mahesh babu", "Jr NTR", "it's remake of some movie and must be of minimal Budget"),
-    new News(104, "Akshay Kumar", "Rajesh1", " Your movies are of low Budget"),
-    new News(101, "Akshay Kumar", "Rajesh2", " Your movies are of minimal Budget")
-     
+    new News(104, "Akshay Kumar", "Rajesh", " Your movies are of low Budget"),
+    new News(101, "Akshay Kumar", "Mahesh", " Your movies are of minimal Budget")
     ); 
 
+
+    //5
     int count = (int) newsList.stream()
 						.filter(x-> x.getComment().contains("Budget"))
 						.count();
 
 		System.out.println(count);
+    System.out.println();
 
-
-       //
-       Optional<News> max1 = newsList.stream() 
-       .collect(Collectors.maxBy(Comparator.comparing(News::getNewsId)));
+      //  //5
+      //  Optional<News> max1 = newsList.stream() 
+      //  .collect(Collectors.maxBy(Comparator.comparing(News::getNewsId)));
        
-       System.out.println(max1.get());
+      //  System.out.println(max1.get());
 
        //6
        Optional<String> max = newsList.stream().map(x->x.getCommentByUser())
 			 			.max((i,j)->i.compareTo(j));
 			 System.out.println(max.get());
+      System.out.println();
 
 
-
-       //7
+//       7
        newsList.stream().map(x->x.getCommentByUser())
 			.sorted()
 			.forEach(System.out::println);
